5b56511d6e3b80e754a22c7bdd52bfd407a189a7
Connor J Smith <connorjsmith101@gmail.com>
2016-02-16 09:39:53 -0500

    Added web-mode for html files

--- init.el
+++ init.el
@@ -107,6 +107,7 @@
 (req-package web-mode
     (add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
     (setq web-mode-content-types-alist '(("jsx"  . "\\.js[x]?\\'")))
     (setq web-mode-enable-auto-pairing t)
     (setq web-mode-enable-css-colorization t)
